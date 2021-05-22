eae8cc6af7dfc9badfad6d155cce5b4644f3b08d
needabreak <paramore77@naver.com>
2021-05-22 19:32:40 +0900

    router 연결 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './pages/main';
+import Page1 from './pages/page1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact={true} path="/" component={Main} />
+        <Route path="/page1" component={Page1} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/page1.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page1 = () => {
+  return (
+    <div>
+      page1.tsx
+    </div>
+  )
+};
+
+export default Page1;
