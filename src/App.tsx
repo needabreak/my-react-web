8471986ff220231034539e91b3fdb3574c80f1f1
needabreak <paramore77@naver.com>
2022-01-11 10:45:39 +0900

    Redux ToolKit 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Main from './pages/main';
 import Page1 from './pages/page1';
+import { store } from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact={true} path="/" component={Main} />
-        <Route path="/page1" component={Page1} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact={true} path="/" component={Main} />
+          <Route path="/page1" component={Page1} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useAppSelector, useAppDispatch } from '../hooks';
 
 const Main = () => {
+  const count = useAppSelector((state) => state.counter.value);
   return (
     <div>
       <strong>main!</strong>
+      <p>{count}</p>
     </div>
   )
 }
